dcd5729628d2ee03541c88201f754491c3ac16dd
Cameron Durham <camerondurham@users.noreply.github.com>
2021-01-14 03:37:09 -0800

    Fix Unix installation script

--- unix/setup.sh
+++ unix/setup.sh
@@ -12,12 +12,18 @@ command -v docker >/dev/null 2>&1 || {
   exit 1
 }
 
-# Pull docker image
-echo "Pulling docker image..."
-docker pull usccsci104/docker:latest || exit $?
+echo "Looking for ch..."
+command -v ch >/dev/null 2>&1 || {
+    echo "Installing ch..."
+    . ./unix/install-ch.sh
+}
+
+# # Pull docker image
+# echo "Pulling docker image..."
+# docker pull usccsci104/docker:latest || exit $?
 
 # Get mount point
-echo "Creating manager script..."
+echo "Preparing csci104 environment..."
 read -r -p "Select a directory to mount: " work
 
 while [[ ! -d "${work}" ]]; do
@@ -27,19 +33,20 @@ done
 
 # Get absolute path of directory
 work="$(cd "${work}" || return; pwd -P)"
-echo "Mount point set, this can be changed later by editing manage.sh..."
+echo "Mount point set, this can be changed later by editing $HOME/.ch.yaml..."
 
 # Create manage scripts
-echo "Creating manager script..."
-rm ${manage} 2> /dev/null
-{
-  echo "#!/usr/bin/env bash";
-  echo ""
-  echo "# Change this to the directory you want to access in your container."
-  echo "work=${work}"
-  echo ""
-  cat ./unix/manage.base.sh
-} >> ${manage};
-chmod +x ${manage}
+echo "Creating csci104 environment..."
+ch create csci104 --image usccsci104/docker:latest --shell /bin/bash --volume "$work:/work" --security-opt seccomp:unconfined --cap-add SYS_PTRACE --replace
+# rm ${manage} 2> /dev/null
+# {
+#   echo "#!/usr/bin/env bash";
+#   echo ""
+#   echo "# Change this to the directory you want to access in your container."
+#   echo "work=${work}"
+#   echo ""
+#   cat ./unix/manage.base.sh
+# } >> ${manage};
+# chmod +x ${manage}
 
 echo "Done!"
